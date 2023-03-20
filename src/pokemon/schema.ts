9788a4b4306dae30ffa877c609730728baf20a13
Mikkel Andersen <mta@trackunit.com>
2023-03-20 14:08:23 +0100

    fix pokemon api

--- src/pokemon/schema.ts
+++ src/pokemon/schema.ts
@@ -1,16 +1,13 @@
 import { SubschemaConfig } from "@graphql-tools/delegate";
 import { loadSchema } from "@graphql-tools/load";
 import { readFileSync } from "fs";
-import { GraphQLSchema } from "graphql";
 import { join } from "path";
 import { makeRemoteExecutor } from "../utils/remote-schema";
 
 const gqlSchema = readFileSync(join(__dirname, "schema.graphqls"), "utf-8");
 
-export const getPokemonSchemaConfig = async (): Promise<
-  SubschemaConfig | GraphQLSchema
-> => {
-  const executor = makeRemoteExecutor("https://graphqlpokemon.favware.tech/v7");
+export const getPokemonSchemaConfig = async (): Promise<SubschemaConfig> => {
+  const executor = makeRemoteExecutor("https://graphql-pokeapi.graphcdn.app");
   const schema = await loadSchema(gqlSchema, { loaders: [] });
   const schemaConfig: SubschemaConfig = {
     schema,
