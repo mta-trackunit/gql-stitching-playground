091065c6d69124623e7e2fa4d5427b30b3a7ecd3
Mikkel Andersen <mta@trackunit.com>
2023-03-20 12:26:37 +0100

    cleanup

--- src/starwars/schema.ts
+++ src/starwars/schema.ts
@@ -1,15 +1,12 @@
 import { SubschemaConfig } from "@graphql-tools/delegate";
 import { loadSchema } from "@graphql-tools/load";
 import { readFileSync } from "fs";
-import { GraphQLSchema } from "graphql";
 import { join } from "path";
 import { makeRemoteExecutor } from "../utils/remote-schema";
 
 const gqlSchema = readFileSync(join(__dirname, "schema.graphqls"), "utf-8");
 
-export const getStarWarsSchemaConfig = async (): Promise<
-  SubschemaConfig | GraphQLSchema
-> => {
+export const getStarWarsSchemaConfig = async (): Promise<SubschemaConfig> => {
   const executor = makeRemoteExecutor(
     "https://swapi-graphql.netlify.app/.netlify/functions/index"
   );
